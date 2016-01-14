681c6bfef3036f807c4b3501d67c8a45c43b44bc
Jens W. Klein <jk@kleinundpartner.at>
2016-01-14 11:59:20 +0100

    fix bootstrap script to not conflict with site-package. this prevent running into https://github.com/kennethreitz/requests/pull/2375

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 
 # see https://community.plone.org/t/not-using-bootstrap-py-as-default/620
 rm -r ./lib ./include ./local ./bin
-virtualenv --clear .
+virtualenv --clear --no-site-packages .
 ./bin/pip install --upgrade pip setuptools zc.buildout
 ./bin/buildout
