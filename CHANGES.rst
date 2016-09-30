fa2af37fb89045771e7ef35467275e91ce9e7b1d
Jens W. Klein <jk@kleinundpartner.at>
2016-09-30 13:35:53 +0200

    Preparing release 1.4

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.4 (unreleased)
+1.4 (2016-09-30)
 ----------------
 
 - New option '-N': feature to hide orphaned without updates.
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '1.4.dev0'
+version = '1.4'
 
 long_description = '{0}\n\n{1}'.format(
     open('README.rst').read(),
